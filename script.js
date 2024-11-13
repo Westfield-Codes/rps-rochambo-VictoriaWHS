f4e58b0995e6d87b03617ea77bd01851da7d9b8e
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-11-13 17:15:21 +0000

    working

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 main();
+
+var moves = ["r", "p", "s"];
+
 function main(){
     let uChoice = 0;
     let cChoice= 0;
@@ -14,7 +17,7 @@ function main(){
 
 function userTurn(){
     let choice = prompt(" enter r,p, or s");
-    if (choice != "r" && choice != "p" && choice != "s"){
+    if (!moves.includes(choice)){
        alert("enter r,p, or s");
        return userTurn();
     }
@@ -25,27 +28,34 @@ function userTurn(){
 
 function cpuTurn(){
     let choice = Math.floor(Math.random()*2);
-    if (choice == 0) return "r";
-    else if (choice == 1) return "p";
-    else if (choice == 2) return "s";
+    alert("I choose "+ choice);
+    return moves[choice];
+    
 }
-
 
 function findWinner(uChoice, cChoice){
-     alert(" finding winner ");
-    let winner = "Undefined";
-    if (uChoice == "r" ){
-        if (cChoice =="s") winner = "player";
-        else winner = "cpu";
-    }
-     if (uChoice == "s" ){
-        if (cChoice =="p") winner = "player";
-        else winner = "cpu";
-    }
-        if (cChoice == "r") winner = "player";
-            else winner ="cpu";
-        alert ("You picked " + uChoice + " and I picked " + cChoice +". " + winner + " won.");
-
+    let winArray=[
+        ["r","p","I"],["r","s","you"],
+        ["p","s","I"],["p","r","you"],
+        ["s","r","I"],["s","p","you"]];
 }
 
+//turns =u+c
+//match =winArray[i][0]+[i][2]
 
+
+   // alert(" finding winner ");
+    //let winner = "Undefined";
+    //if (uChoice == "r" ){
+   //     if (cChoice =="s") winner = "player";
+   //     else winner = "cpu";
+   // }
+   //  if (uChoice == "s" ){
+   //     if (cChoice =="p") winner = "player";
+   //     else winner = "cpu";
+   // }
+    //    if (cChoice == "r") winner = "player";
+    //        else winner ="cpu";
+   //     alert ("You picked " + uChoice + " and I picked " + cChoice +". " + winner + " won.");
+
+}
