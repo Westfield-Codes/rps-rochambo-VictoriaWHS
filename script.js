a3e0ec6710d30940875eff9d53bb5185f9c7a39e
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-11-18 17:12:18 +0000

    broken

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ main();
 function main(){
     let score = [0,0]
     let winner = 2;
+    let round = 1;
     let rounds = setRounds();
     while (round <= rounds){
         winner = rpsRound();
-        score[winner]++
+        score[winner]++;
+        round++
     }
     alert("You have " + score[0] + " and I have"+  score[1]);
     if (score[0]>score[1]) alert("You win!");
@@ -15,36 +17,40 @@ function main(){
 
 function setRounds(){
     let rounds = prompt("How many rounds?");
-    if (rounds % 2 ===0) alert("must be odd");
+    if (rounds % 2 === 0) {
+        alert("must be odd");
+        return setRounds();
+    }
     else return rounds;
 }
 function rpsRound(){
     let uChoice = 0;
-    let cChoice =0;
+    let cChoice = 0;
     while (uChoice == cChoice){
-        let uChoice= userTurn();
-        let cChoice= cpuTurn();
+        uChoice= userTurn();
+        cChoice= cpuTurn();
         if (uChoice == cChoice) alert ("we both choose " + cChoice);
         else return rpsRound();
     }
     let winner = findWinner(c,u);
     let players = ["you","i"];
-    let win= players.indexOf(winner);
+    let win = players.indexOf(winner);
     return win;
 }
 
 function userTurn(){
     moves = ["r","p","s"];
     let choice = prompt("enter r p or s.");
-    if (moves.includes(choice)) 
-        return choice;
-    else alert("choose r p or s")
-    return userTurn();
+    if (moves.includes(choice)) return choice;
+    else {
+        alert("choose r p or s it is round " + rounds)
+        return userTurn();
+    }
 }
 
 function cpuTurn(){
     let moves = ["r","p","s"];
-    let choice = Math.floor(math.random()*3);
+    let choice = Math.floor(Math.random()*3);
     return moves[choice];
 }
 
@@ -55,6 +61,7 @@ function findWinner(u, c){
                 winner= winArray[i][2];
 
             }
+            alert(" got to find winner will write all later");
         }
-       
+
 }
