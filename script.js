16aac8cf392b6d554126f8498b124e2a1b728cb0
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-11-18 16:28:23 +0000

    array

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
 main();
-
 function main(){
-    let score = [0,0]
-    let winner = 2;
-    //or 3?//
-    let rounds = setRounds();
-    while (round <= rounds){
-        winner = rpsRound();
-        score[winner]++
-    }
-    alert("You have " + score[0] + " and I have"+  score[1]);
-    if (score[0]>score[1]) alert("You win!");
-    else alert("I win!");
-}
-
-function setRounds(){
-    let rounds = propmt("How many rounds?");
-    if (rounds % 2 ===0) alert("must be odd");
-    //fix this line above //
-    else return rounds;
-}
-function rpsRound(){
     let uChoice = 0;
-    let cChoice =0;
+    let cChoice = 0;
     while (uChoice == cChoice){
-        let uChoice= userTurn();
-        let cChoice= cpuTurn();
+        uChoice = userTurn();
+        cChoice = cpuTurn();
         if (uChoice == cChoice) alert ("we both choose " + cChoice);
-        else return rpsRound();
     }
-    let winner = findWinner(c,u);
-    let players = ["you","i"];
-    let win= players.indexOf(winner);
-    return win;
+    findWinner(uChoice,cChoice);
 }
 
 function userTurn(){
     moves = ["r","p","s"];
     let choice = prompt("enter r p or s.");
-    if (moves.includes(choice)) 
+    if (moves.includes(choice)){
+        console.log(choice);
         return choice;
-    else alert("choose r p or s")
-    return userTurn();
+    } 
+    else {
+        alert("choose r p or s")
+        return userTurn();
+    }
 }
 
 function cpuTurn(){
     let moves = ["r","p","s"];
-    let choice = Math.floor(math.random()*2)+1;
+    let choice = Math.floor(Math.random()*3);
     return moves[choice];
 }
 
@@ -55,8 +34,7 @@ function findWinner(u, c){
         for (let i = 0; i< winArray.length; i++){
             if (winArray[i][0] == u && winArray[i][1]==c){
                 winner= winArray[i][2];
-
             }
-        }
-       
+        } 
+    alert(" you picked " + u + " and I picked " + c + " the winner is " + winner);
 }
